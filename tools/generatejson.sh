03f0ef686556b51aa28e9a884300a323a8ae3ab6
Jhonatan_OSS <luis.vm0401@gmail.com>
2024-05-23 08:35:57 +0200

    tools: Relocate .json and rom

--- tools/generatejson.sh
+++ tools/generatejson.sh
@@ -1,48 +1,54 @@
 #!/bin/bash
 #
-# Copyright (C) 2023 ivanmeler <i_ivan@windowslive.com>
-# Copyright (C) 2024 Panagiotis Englezos <panagiotisegl@gmail.com>
+# Android ROM .json file generator
 #
-# Android ROM .json file generator 
-#
-# Usage: ./generatejson.sh <ota zip>
+# Usage: ./generatejson.sh <json output> <ota zip>
 #
 
-if [ -z "$1" ] ; then
-    echo "Usage: $0 <ota zip>"
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: $0 <json output> <ota zip>"
     exit 1
 fi
 
-ROM="$1"
+JSON_FILE="$1"
+ROM="$2"
+
+if [ ! -f "$ROM" ]; then
+    echo "Error: ROM file not found!"
+    exit 1
+fi
 
 METADATA=$(unzip -p "$ROM" META-INF/com/android/metadata)
 SDK_LEVEL=$(echo "$METADATA" | grep post-sdk-level | cut -f2 -d '=')
 TIMESTAMP=$(echo "$METADATA" | grep post-timestamp | cut -f2 -d '=')
 
-FILENAME=$(basename $ROM)
-DEVICE=$(echo $FILENAME | cut -f5 -d '-' | cut -f1 -d".")
-ROMTYPE=$(echo $FILENAME | cut -f4 -d '-')
-DATE=$(echo $FILENAME | cut -f3 -d '-')
-ID=$(echo ${TIMESTAMP}${DEVICE}${SDK_LEVEL} | sha256sum | cut -f 1 -d ' ')
-SIZE=$(du -b $ROM | cut -f1 -d '	')
-TYPE=$(echo $FILENAME | cut -f4 -d '-')
-VERSION=$(echo $FILENAME | cut -f2 -d '-')
-RELASE_TAG=lineage-${VERSION}-${DATE}-${ROMTYPE}-${DEVICE}
+FILENAME=$(basename "$ROM")
+DEVICE=$(echo "$FILENAME" | cut -f5 -d '-' | cut -f1 -d '.')
+ROMTYPE=$(echo "$FILENAME" | cut -f4 -d '-')
+DATE=$(echo "$FILENAME" | cut -f3 -d '-')
+ID=$(echo "${TIMESTAMP}${DEVICE}${SDK_LEVEL}" | sha256sum | cut -f 1 -d ' ')
+SIZE=$(du -b "$ROM" | cut -f1)
+VERSION=$(echo "$FILENAME" | cut -f2 -d '-')
+RELEASE_TAG="lineage-${VERSION}-${DATE}-${ROMTYPE}-${DEVICE}"
 
-URL="https://github.com/kevin-btw/LineageOS-Releases/download/${RELASE_TAG}/${FILENAME}"
+#  URL to point to the releases page
+URL="https://github.com/kevin-btw/LineageOS-Releases/releases"
 
-response=$(jq -n --arg datetime $TIMESTAMP \
-        --arg filename $FILENAME \
-        --arg id $ID \
-        --arg romtype $ROMTYPE \
-        --arg size $SIZE \
-        --arg url $URL \
-        --arg version $VERSION \
+response=$(jq -n --arg datetime "$TIMESTAMP" \
+        --arg filename "$FILENAME" \
+        --arg id "$ID" \
+        --arg romtype "$ROMTYPE" \
+        --arg size "$SIZE" \
+        --arg url "$URL" \
+        --arg version "$VERSION" \
         '$ARGS.named'
 )
 wrapped_response=$(jq -n --argjson response "[$response]" '$ARGS.named')
 
-echo "$wrapped_response" > ../ota/$VERSION/$DEVICE.json
-git add ../ota/$VERSION/$DEVICE.json
+JSON_OUTPUT_DIR=$(dirname "$JSON_FILE")
+
+mkdir -p "$JSON_OUTPUT_DIR"
+echo "$wrapped_response" > "$JSON_FILE"
+git add "$JSON_FILE"
 git commit -m "Update json file for $DEVICE-$VERSION-${DATE}"
 git push -f
